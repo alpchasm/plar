318f5cc519798e20db474eab392c2eff1b4609fd
Tom Rodman <rodmant@epjdn.zq3q.org>
2025-10-18 06:50:48 -0500

    Minor code fix. Now have git short commit hash reporting ok.

--- js/plsPlayer.js
+++ js/plsPlayer.js
@@ -196,8 +196,7 @@ class PLSPlayer {
     setupEventListeners() {
         this.container.querySelector('#about').addEventListener('click', () => {
             this.clearShareUrl();
-            alert(`PLS Playlist or Audio Player v${PLSPlayer.version}\nPlaylist Last Modified: ${this.playlistLastModified}\n\nSupports PLS playlists or single audio files (MP3/MP4) with play/pause, next/prev, speed controls, and segment playback via #t=start,end or #t=HH:MM:SS,HH:MM:SS. Use arrow keys for navigation and 'SPACE', 's', 'e', 'b', 'n', 'p', 'P', 'j', 'J', 'c' keys for control. HTTP audio may not play on HTTPS pages; use HTTP player URL: ${this.getHttpPlayerUrl()}.`);
-            alert(`Git Commit: ${this.gitCommit}`);
+            alert(`PLS Playlist or Audio Player Git Commit: ${this.gitCommit} v${PLSPlayer.version}\nPlaylist Last Modified: ${this.playlistLastModified}\n\nSupports PLS playlists or single audio files (MP3/MP4) with play/pause, next/prev, speed controls, and segment playback via #t=start,end or #t=HH:MM:SS,HH:MM:SS. Use arrow keys for navigation and 'SPACE', 's', 'e', 'b', 'n', 'p', 'P', 'j', 'J', 'c' keys for control. HTTP audio may not play on HTTPS pages; use HTTP player URL: ${this.getHttpPlayerUrl()}.`);
         });
 
         this.container.querySelector('#help').addEventListener('click', () => {
